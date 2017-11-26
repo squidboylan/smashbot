ce1cbe1e54346f285f0701f483730b4c51c7c844
Caleb Boylan <calebboylan@gmail.com>
2017-11-26 10:34:46 -0800

    Add run and build scripts

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /var/lib/dbus:/var/lib/dbus -v ~/.pulse:/root/.pulse -v /dev/shm:/dev/shm \
+    -v /etc/machine-id:/etc/machine-id \
+    -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+    --device /dev/dri:/dev/dri \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --device /dev/snd \
+    --group-add $(getent group audio | cut -d: -f3) \
+    -v /home/squid/.local/share/dolphin-emu/Games:/home/developer/.local/share/dolphin-emu/Games \
+    -v /home/squid/.config/dolphin-emu:/home/developer/.config/dolphin-emu \
+    smashbot
